f90c70ed9d395ae9b289adaebafd4278a71f8d61
S1GSKim <89263033+S1GSKim@users.noreply.github.com>
2022-11-11 15:01:40 +0800

    updated files

--- Show_Recommendation/server_c.js
+++ Show_Recommendation/server_c.js
@@ -8,9 +8,6 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 let accesskeyid = process.env.accesskeyid;
 let secretaccesskey = process.env.secretaccesskey;
-console.log(accesskeyid);
-console.log(secretaccesskey);
-
 
 import { PersonalizeRuntimeClient, GetRecommendationsCommand } from "@aws-sdk/client-personalize-runtime"; // ES Modules import
 
@@ -22,6 +19,46 @@ app.get('/', function (req, res) {
   const __dirname = dirname(__filename);
   res.sendFile( __dirname + "/" + "test_c.html" );
 })
+//###########################################################
+// Create service client module using ES6 syntax.
+import { S3Client } from "@aws-sdk/client-s3";
+// Set the AWS Region.
+const REGION = "us-east-1";
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({ region: REGION });
+export { s3Client };
+
+// Import required AWS SDK clients and commands for Node.js.
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import * as path from 'path';
+import * as fs from 'fs';
+app.get('/UploadS3', function (req, res) {
+  // Path to and name of object. For example '../myFiles/index.js'.
+  const file = req.query.filename;
+  const fileStream = fs.createReadStream(file);
+
+  // Set the parameters
+  const uploadParams = {
+    Bucket: "is459-grp8",
+    // Add the required 'Key' parameter using the 'path' module.
+    Key: "Import_Dataset_Job/"+path.basename(file),
+    // Add the required 'Body' parameter
+    Body: fileStream,
+  };
+
+  // Upload file to specified bucket.
+  const run = async () => {
+    try {
+      const data = await s3Client.send(new PutObjectCommand(uploadParams));
+      console.log("Success", data);
+      return data; // For unit tests.
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+  run();
+
+});
 
 app.get('/rec4u', function (req, res) {
   // Prepare output in JSON format
@@ -50,7 +87,11 @@ app.get('/rec4u', function (req, res) {
     result.forEach(item => imageURLs.push(dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"));
     // console.log(imageURLs);
     res.setHeader('Content-type','text/html')
-    imageURLs.forEach(image => res.write('<img src='+image+'>'));
+    res.write('<head><link rel = "stylesheet" href "https://bootswatch.com/4/cyborg/bootstrap.min.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></head><h2 style="color:blue; text-align:center;">Recommendations</h2><div class="row">')
+    result.forEach(item => res.write('<div class="col-2"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"+'><div class="card-body"><h5 class="card-title">'+item.itemId+'</h5></div></div></div>'));
+    // imageURLs.forEach(image => res.write('<div class="col-1"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+image+'></div></div>'));
+    // <div class="card-body"><h5 class="card-title">'+item.itemID+'</h5></div>
+    res.write('</div>')
   }).catch(console.error.bind(console))
 });
 
@@ -81,7 +122,11 @@ app.get('/MostViewed1', function (req, res) {
     result.forEach(item => imageURLs.push(dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"));
     // console.log(imageURLs);
     res.setHeader('Content-type','text/html')
-    imageURLs.forEach(image => res.write('<img src='+image+'>'));
+    res.write('<head><link rel = "stylesheet" href "https://bootswatch.com/4/cyborg/bootstrap.min.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></head><h2 style="color:blue; text-align:center;">Most Viewed Items</h2><div class="row">')
+    result.forEach(item => res.write('<div class="col-2"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"+'><div class="card-body"><h5 class="card-title">'+item.itemId+'</h5></div></div></div>'));
+    // imageURLs.forEach(image => res.write('<div class="col-1"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+image+'></div></div>'));
+    // <div class="card-body"><h5 class="card-title">'+item.itemID+'</h5></div>
+    res.write('</div>')
   }).catch(console.error.bind(console))
 });
 
@@ -114,7 +159,11 @@ app.get('/AlsoView1', function (req, res) {
     result.forEach(item => imageURLs.push(dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"));
     // console.log(imageURLs);
     res.setHeader('Content-type','text/html')
-    imageURLs.forEach(image => res.write('<img src='+image+'>'));
+    res.write('<head><link rel = "stylesheet" href "https://bootswatch.com/4/cyborg/bootstrap.min.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></head><h2 style="color:blue; text-align:center;">Also Viewed Items</h2><div class="row">')
+    result.forEach(item => res.write('<div class="col-2"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"+'><div class="card-body"><h5 class="card-title">'+item.itemId+'</h5></div></div></div>'));
+    // imageURLs.forEach(image => res.write('<div class="col-1"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+image+'></div></div>'));
+    // <div class="card-body"><h5 class="card-title">'+item.itemID+'</h5></div>
+    res.write('</div>')
   }).catch(console.error.bind(console))
 });
 
@@ -145,7 +194,11 @@ app.get('/BestSeller1', function (req, res) {
     result.forEach(item => imageURLs.push(dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"));
     // console.log(imageURLs);
     res.setHeader('Content-type','text/html')
-    imageURLs.forEach(image => res.write('<img src='+image+'>'));
+    res.write('<head><link rel = "stylesheet" href "https://bootswatch.com/4/cyborg/bootstrap.min.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></head><h2 style="color:blue; text-align:center;">Best Sellers</h2><div class="row">')
+    result.forEach(item => res.write('<div class="col-2"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"+'><div class="card-body"><h5 class="card-title">'+item.itemId+'</h5></div></div></div>'));
+    // imageURLs.forEach(image => res.write('<div class="col-1"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+image+'></div></div>'));
+    // <div class="card-body"><h5 class="card-title">'+item.itemID+'</h5></div>
+    res.write('</div>')
   }).catch(console.error.bind(console))
 });
 
@@ -177,8 +230,13 @@ app.get('/BoughtTgt1', function (req, res) {
     const imageURLs = []
     const dir = "https://is459-grp8.s3.amazonaws.com/images/0"
     result.forEach(item => imageURLs.push(dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"));
+    // console.log(imageURLs);
     res.setHeader('Content-type','text/html')
-    imageURLs.forEach(image => res.write('<img src='+image+'>'));
+    res.write('<head><link rel = "stylesheet" href "https://bootswatch.com/4/cyborg/bootstrap.min.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></head><h2 style="color:blue; text-align:center;">Frequently bought together Items</h2><div class="row">')
+    result.forEach(item => res.write('<div class="col-2"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+dir+item.itemId.substring(0,2)+"/0"+item.itemId+".jpg"+'><div class="card-body"><h5 class="card-title">'+item.itemId+'</h5></div></div></div>'));
+    // imageURLs.forEach(image => res.write('<div class="col-1"><div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); padding: 16px;text-align: center;"><img class="card-img-top" src='+image+'></div></div>'));
+    // <div class="card-body"><h5 class="card-title">'+item.itemID+'</h5></div>
+    res.write('</div>')
     // console.log(imageURLs);
     // res.json(imageURLs);
 
